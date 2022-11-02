4da4ef2198ce734f6b95127f9f7dd373895dc8ae
Lorraine <lorrainecannom@gmail.com>
2022-11-02 12:12:24 -0400

    Algorithm with ArrayDeque and Stack

--- src/main/java/com/cydeo/LengthOfLastWord/LengthOfWord.java
+++ src/main/java/com/cydeo/LengthOfLastWord/LengthOfWord.java
@@ -7,14 +7,15 @@ import java.util.Stack;
 
 public class LengthOfWord {
     public static void main(String[] args) {
-        String str="Hello world";
+        String str="Hello world ";
 
         System.out.println(lengthOfLastWord(str));
 
     }
 
-    public static int lengthOfLastWord(String str){
+    public static String gettingLastWord(String str){
 
+        ArrayDeque<String> unorderedWord=new ArrayDeque<>();
         Stack<String> sentence= new Stack<>();
         String lastWord="";
 
@@ -22,10 +23,28 @@ public class LengthOfWord {
             sentence.push(""+str.charAt(i));
         }
         while(!sentence.peek().equals(" ")){
-           lastWord+=sentence.pop();
+            unorderedWord.add(sentence.pop());
         }
-        return lastWord.length();
+
+        while(!unorderedWord.isEmpty()){
+            lastWord+=unorderedWord.removeLast();
+        }
+
+        return lastWord;
     }
 
+
+    public static int lengthOfLastWord(String s){
+        int p=s.length()-1;
+        while(p>=0 && s.charAt(p)==' '){
+            p--;
+        }
+        int length=0;
+        while(p>=0 && s.charAt(p)!=' '){
+            p--;
+            length++;
+        }
+        return length;
+    }
 
 }
