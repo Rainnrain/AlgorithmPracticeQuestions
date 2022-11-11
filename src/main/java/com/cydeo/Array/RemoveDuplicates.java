6412bb8550d0a16934631d47a7708bb8c3f4790e
Lorraine <lorrainecannom@gmail.com>
2022-11-11 17:08:57 -0500

    Algorithm with Trees

---
+++ src/main/java/com/cydeo/Array/RemoveDuplicates.java
@@ -0,0 +1,30 @@
+package com.cydeo.Array;
+
+import java.util.Arrays;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int [] nums = {1};
+
+removeDuplicates(nums);
+    }
+
+
+
+    public static void removeDuplicates(int [] nums){
+
+      int counter=1;
+
+        for (int i = 1; i < nums.length; i++) {
+
+
+            if(nums[i-1]!=nums[i]){
+                nums[counter]=nums[i];
+                counter++;
+            }
+
+
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+}
