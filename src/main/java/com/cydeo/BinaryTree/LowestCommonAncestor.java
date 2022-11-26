443228c0ae05a3ae795481abdda91d593374be57
Lorraine <lorrainecannom@gmail.com>
2022-11-26 09:52:34 -0500

    Recursion practice with BST

---
+++ src/main/java/com/cydeo/BinaryTree/LowestCommonAncestor.java
@@ -0,0 +1,21 @@
+package com.cydeo.BinaryTree;
+
+public class LowestCommonAncestor {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+//        if((root.val==p.val && root.val<q.val)||(root.val==p.val && root.val>q.val)) return root;
+//        if((root.val==q.val && root.val<p.val)||(root.val==q.val && root.val>p.val)) return root;
+//        if(root.val>p.val && root.val< q.val) return root;
+//        if(root.val<p.val && root.val> q.val) return root;
+
+
+        if(root.val> p.val && root.val>q.val) return lowestCommonAncestor(root.left, p, q);
+        else if(root.val< p.val && root.val< q.val) return lowestCommonAncestor(root.right, p,q);
+        return root;
+    }
+}
