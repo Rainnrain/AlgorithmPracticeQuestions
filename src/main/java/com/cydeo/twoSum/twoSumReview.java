470ec7046f05a9dce1efad2052f82be29d412382
Lorraine <lorrainecannom@gmail.com>
2022-12-08 20:23:46 -0500

    ALgo practice

---
+++ src/main/java/com/cydeo/twoSum/twoSumReview.java
@@ -0,0 +1,38 @@
+package com.cydeo.twoSum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class twoSumReview {
+
+    public static void main(String[] args) {
+
+        int [] num= {3,2,4};
+        System.out.println(Arrays.toString(sumOfTwo(num, 6)));
+    }
+
+
+    public static int[] sumOfTwo(int [] arr, int target) {
+
+        Arrays.sort(arr);
+        int pointer1 = 2;
+        int pointer2 = 0;
+
+
+      while(arr[pointer1]+arr[pointer2]!=target){
+
+          if(arr[pointer1]+arr[pointer2]<target) {
+              pointer1++;
+
+          }else if(arr[pointer1]+arr[pointer2]>target){
+              pointer1--;
+          }
+         if( arr[pointer1]+arr[pointer2]!=target ) pointer2++;
+      }
+      return new int [] {arr[pointer1], arr[pointer2]};
+    }
+
+
+
+}
