bd27fc128d5ece6423efe946f5f696f1adf20bf5
Lorraine <lorrainecannom@gmail.com>
2023-02-23 10:41:31 -0500

    ALgo practice

---
+++ src/main/java/com/cydeo/String/ReverseWords.java
@@ -0,0 +1,26 @@
+package com.cydeo.String;
+
+public class ReverseWords {
+
+    public String reverseWords(String s) {
+        String [] strAr=s.split(" ");
+        StringBuilder rev= new StringBuilder();
+
+        for(int i=strAr.length-1; i>=0 ;i--){
+            if(!strAr[i].isBlank()){
+                rev.append(strAr[i]).append(" ");
+            }
+        }
+        return rev.toString().trim();
+    }
+
+    public String reverseWords2(String s) {
+        String[] arr = s.split(" ");
+        StringBuilder str = new StringBuilder();
+
+        for (int i = arr.length-1; i >= 0; i--) {
+            if (arr[i].length()>0) str.append(arr[i]+" ");
+        }
+        return str.toString().trim();
+    }
+}
